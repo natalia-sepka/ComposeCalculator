ddac24607dd58d499e2379d495033d1eb910d7e8
natalia.sepka <natalia.sepka@gmail.com>
2023-06-29 10:46:16 +0200

    Initial commit

---
+++ app/src/main/java/pl/sepka/composecalculator/MainActivity.kt
@@ -0,0 +1,16 @@
+package pl.sepka.composecalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import pl.sepka.composecalculator.ui.theme.ComposeCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCalculatorTheme {
+            }
+        }
+    }
+}
