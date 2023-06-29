236722cd22c9994b4aedde15e2e41e1567a8b17f
natalia.sepka <natalia.sepka@gmail.com>
2023-06-29 11:27:19 +0200

    Added Calculator State and View Model

--- app/src/main/java/pl/sepka/composecalculator/MainActivity.kt
+++ app/src/main/java/pl/sepka/composecalculator/MainActivity.kt
@@ -3,6 +3,8 @@ package pl.sepka.composecalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import pl.sepka.composecalculator.ui.theme.ComposeCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,6 +12,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCalculatorTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
             }
         }
     }
